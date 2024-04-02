53e9c3066965075678602f86412a75bca7ad584b
Yevgeniy <a123@123s-MacBook-Pro.local>
2024-04-02 18:13:19 +0300

    added corrections

--- Lesson_8_Dictionary/Homework_8.py
+++ Lesson_8_Dictionary/Homework_8.py
@@ -10,6 +10,16 @@ if name_roles:
     print(f'Hello, {' & ' .join(name_roles)}!')
 else:
     print('Hello, Guest!')
+                                  # Marina's solution
+#                                 name = input('Please, enter your name: ').title()
+#                                 roles_dict = {'admin' : ['Evgeniy', 'Dmitry', 'asd'], 'maintainer' : ["Marina", "Sergey"],
+#                                           'manager' : ["Anastasia", "Dmitry", 'asd'], 'developer': ["Evgeniy"]}
+#                                 for role, names in roles_dict.items():
+#                                     if name in names:
+#                                         print(f'Hello, {role}!')
+#                                         break
+#                                 else:
+#                                     print('Hello, Guest!')
 
 # Task_2 add dictionary to string
 params = {'v': 'some_id', 'list': 'some_list', 'index': '6', 't': '0s'}
@@ -17,7 +27,9 @@ initial_str = 'https://www.youtube.com/watch?'
 result_link = initial_str
 for key, values in params.items():
     result_link += f'{key}={values}&'
-result_link = result_link[:-1:]
+#                                 # result_link = result_link[:-1:] OLD DECISION
+# Marina's solution
+result_link = result_link.strip('&')
 print(result_link)
 
 # Task_3 set to dictionary
@@ -35,4 +47,12 @@ for key, value in result_dict.items():
     row += 1
     if row % 5 == 0:
         print()
+
+                                # Marina's solution
+                                # result_link = 'https://www.youtube.com/watch?v=some_id&list=some_list&index=6&t=0s'
+                                # dict_count = {}
+                                # for element in result_link:
+                                #     if element not in dict_count:
+                                #         dict_count [element] = result_link. count (element)
+                                #         print(f'How many times an element is repeated in a line:\n{dict_count}')
 
