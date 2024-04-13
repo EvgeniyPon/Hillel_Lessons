c638a6a0bf4ce21a0413d8b81cac7590682b46ae
Yevgeniy <a123@123s-MacBook-Pro.local>
2024-04-13 20:02:54 +0300

    my_test

--- Tasks_from_book/task_1.py
+++ Tasks_from_book/task_1.py
@@ -1,3 +1,51 @@
 # 1.6 page 24
-# 1 email
+#1 email
+# print('Evgeniy')
+# print('Ukraine')
+#
+# #2 Greetings
+# name = input("What is you name? ")
+# print(f'Hello, {name}!')
+#
+# #3 square
+# lenght = float(input("Input the length in meters: "))
+# width = float(input('Input the width in meters: '))
+# square = lenght * width
+# print(f'the square of your room is {square} of meters')
+
+#4 Acre-foot
+# lenght = float(input("Input the length in foot: "))
+# width = float(input('Input the width in foot: '))
+# square = lenght * width
+# print(f'the square of your field is {square / 43560} of acre')
+
+#5 homless busines
+# cheep_bottle = 0.1
+# expensive_bottle = 0.25
+# small_bottle = int(input('How many bottles of capacity less then 1L did yoy bring, my friend? '))
+# big_bottle = int(input('ok, i got it, my friend! '
+#                        'and how many bottles of capacity more then 1L did yoy bring? '))
+# print(f'You can get {small_bottle * cheep_bottle + big_bottle * expensive_bottle:.2f}$ for this')
+
+#6 tax & tip
+# spend = float(input("how much did you spend? "))
+# tax = spend * 20 / 100
+# tip = (spend - tax) * 18 / 100
+# print(f'tax = {tax}, tip = {tip}, total = {spend + tax + tip}')
+
+#7 summ of positive numbers
+# number = int(input("input a number "))
+# for n in range(1, number):
+#     total = n * (n + 1) / 2
+# print(int(total))
+
+#8 shop
+# souvenir_weight = 0.75
+# trinkets_weight = 0.112
+# souvenir_quantity = int(input('How many souvenirs have you bought?: '))
+# trinkets_quantity = int(input('How many trinkets have you bought?: '))
+# print(f'Total weight of your package is'
+#       f' {souvenir_weight * souvenir_quantity + trinkets_weight * trinkets_quantity} kg')
+
+#9 Compound interest
 
